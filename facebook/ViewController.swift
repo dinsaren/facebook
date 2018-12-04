da06742f35e9ef025fa1bc29e88e12e852e88b9e
dinsaren <dinsarenkh@gmail.com>
2018-12-04 13:37:54 +0700

    Initial Commit

---
+++ facebook/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  facebook
+//
+//  Created by Dinsaren on 12/4/18.
+//  Copyright © 2018 Dinsaren. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ facebookTests/facebookTests.swift
@@ -0,0 +1,34 @@
+//
+//  facebookTests.swift
+//  facebookTests
+//
+//  Created by Dinsaren on 12/4/18.
+//  Copyright © 2018 Dinsaren. All rights reserved.
+//
+
+import XCTest
+@testable import facebook
+
+class facebookTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
