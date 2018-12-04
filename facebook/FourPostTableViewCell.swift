9ee0f7ce1bc13880fec7963f4d618d3d94056efe
dinsaren <dinsarenkh@gmail.com>
2018-12-04 19:29:51 +0700

    first commit

---
+++ facebook/FourPostTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  FourPostTableViewCell.swift
+//  facebook
+//
+//  Created by Dinsaren on 12/4/18.
+//  Copyright © 2018 Dinsaren. All rights reserved.
+//
+
+import UIKit
+
+class FourPostTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ facebook/ScondePostTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ScondePostTableViewCell.swift
+//  facebook
+//
+//  Created by Dinsaren on 12/4/18.
+//  Copyright © 2018 Dinsaren. All rights reserved.
+//
+
+import UIKit
+
+class ScondePostTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
